1a4bfe18abd9d9143fa062117317a542b50e280b
Luka <luka.ivanovski.h63689@mcast.edu.mt>
2026-04-22 10:45:26 +0200

    Update LI_storage_client.py
    Finished up the storage client side of the code, which only leaves me with the storage functions file to finish off in the coming days and then i'll update the readme file on how to run my part of the code.

--- LI_storage_client.py
+++ LI_storage_client.py
@@ -11,4 +11,50 @@ ss.listen()
 
 print("The storage client is running and waiting for a connection...")
 
+try:
+    ns, addr = ss.accept()  #This will wait for User Program to connect to the Storage Client
+    print("User Program connected from", addr)
+
+    while True:  #This will keep receiving and handling commands until the EXIT option is chosen 
+        try:
+            message = ns.recv(1024)
+            message = message.decode()
+
+            if message.startswith("SAVE"):    #This handles the file saving to disk section
+                parts = message.split(" ", 2)
+                filename = parts[1]
+                filedata = parts[2]
+                save_file(ns, filename, filedata)
+
+            elif message.startswith("READ"):    #This will handle the file reading and sending it back 
+                parts = message.split(" ", 1)
+                filename = parts[1]
+                read_file(ns, filename)
+
+            elif message.startswith("DELETE"):     #This handles the deletion of a file from the disk
+                parts = message.split(" ", 1)
+                filename = parts[1]
+                delete_file(ns, filename)
+
+            elif message.startswith("RENAME"):     #This handles the renaming of a file inside the disk
+                parts = message.split(" ", 2)
+                oldname = parts[1]
+                newname = parts[2]
+                rename_file(ns, oldname, newname)
+
+            elif message == "EXIT":     #This handles the disconnection and stops the loops
+                print("User Program disconnected.")
+                break
+
+            else:    #This will handle any unrecognised commands
+                response = "UNKNOWN_COMMAND"
+                ns.send(response.encode())
+
+        except:
+            print("Error receiving message from User Program.")
+            break
+
+except:
+    print("Error accepting connection.")
+
 #TBC
