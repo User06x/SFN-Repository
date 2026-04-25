5c5ccc26ef7b2e1b4e81eac69ada4df1c5b692d5
Luka <luka.ivanovski.h63689@mcast.edu.mt>
2026-04-25 08:18:24 +0200

    Update LI_storage_functions.py
    Adding the rest of the code for the storage function file to finalize it.

--- LI_storage_functions.py
+++ LI_storage_functions.py
@@ -15,4 +15,33 @@ def save_file(conn, filename, filedata):
         response = "SAVE_ERROR"
         conn.send(response.encode())
 
-#TBC
+def read_file(conn, filename):
+    #This will read a file from the disk and then it will send the content back
+    try:
+        f = open(filename, "r")
+        filedata = f.read()
+        f.close()
+        conn.send(filedata.encode())
+    except:
+        response = "READ_ERROR"
+        conn.send(response.encode())
+
+def delete_file(conn, filename):
+    #This will delete a file from disk, returning back an 'OK' or 'ERROR' text
+    try:
+        os.remove(filename)
+        response = "DELETE_OK"
+        conn.send(response.encode())
+    except:
+        response = "DELETE_ERROR"
+        conn.send(response.encode())
+
+def rename_file(conn, oldname, newname):
+    #This will rename a file on the disk, return it back with 'OK' or 'ERROR' text
+    try:
+        os.rename(oldname, newname)
+        response = "RENAME_OK"
+        conn.send(response.encode())
+    except:
+        response = "RENAME_ERROR"
+        conn.send(response.encode())
