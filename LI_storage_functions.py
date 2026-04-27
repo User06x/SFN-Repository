f669d08c9fa471cd1032259541b3f251e457f6a3
Luka <luka.ivanovski.h63689@mcast.edu.mt>
2026-04-27 07:42:23 +0200

    Update LI_storage_functions.py
    Tweaking the storage functions file to match the format protocol of the user program module

--- LI_storage_functions.py
+++ LI_storage_functions.py
@@ -1,47 +1,61 @@
-#This is my storage functions python file for the distributed file system
-#This module will contain all the file system functions that will be used for the Storage Client file
+#This is the my storage client python file for the distributed file system
+#This file will be in charge of accepting connections from the User Program system and handle the file operations.
 
-import os  #This will be used for the file operations like deleting and renaming 
+import socket  #This is used to create the server and handle connections
+from storage_functions import save_file, read_file, delete_file, append_file  #This imports functions from my storage functions module
 
-#This saves the received data into a file on the disk, then sends back 'OK' or 'ERROR' depending on the outcome.
-def save_file(conn, filename, filedata):
-    try:
-        f = open(filename, "w")
-        f.write(filedata)
-        f.close()
-        response = "SAVE_OK"
-        conn.send(response.encode())
-    except:
-        response = "SAVE_ERROR"
-        conn.send(response.encode())
+#This will set up the server socket and will begin to start listening on port number 6001
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind(('127.0.0.1', 6001))
+ss.listen()
 
-def read_file(conn, filename):
-    #This will read a file from the disk and then it will send the content back
-    try:
-        f = open(filename, "r")
-        filedata = f.read()
-        f.close()
-        conn.send(filedata.encode())
-    except:
-        response = "READ_ERROR"
-        conn.send(response.encode())
+print("Storage Client is running and waiting for connection...")
 
-def delete_file(conn, filename):
-    #This will delete a file from disk, returning back an 'OK' or 'ERROR' text
-    try:
-        os.remove(filename)
-        response = "DELETE_OK"
-        conn.send(response.encode())
-    except:
-        response = "DELETE_ERROR"
-        conn.send(response.encode())
+try:
+    ns, addr = ss.accept()  #This will wait for User Program to connect to the Storage Client
+    print("User Program connected from", addr)
 
-def rename_file(conn, oldname, newname):
-    #This will rename a file on the disk, return it back with 'OK' or 'ERROR' text
-    try:
-        os.rename(oldname, newname)
-        response = "RENAME_OK"
-        conn.send(response.encode())
-    except:
-        response = "RENAME_ERROR"
-        conn.send(response.encode())
+    while True:  #This will keep looping and waiting for commands from the User Program module
+        try:
+            message = ns.recv(1024)
+            message = message.decode()
+            message = message.strip()   #This will remove the '\n' at the end of the message
+
+            parts = message.split("|")  #This will split the message by a pipe separator
+            command = parts[0]          #The first part is always the command
+
+            if command == "save":        #This will handle saving a file to the disk
+                filename = parts[1]
+                filedata = parts[2]
+                save_file(ns, filename, filedata)
+
+            elif command == "read":      #This will handle reading a file and sending it back to the user
+                filename = parts[1]
+                read_file(ns, filename)
+
+            elif command == "delete":    #This will handle the deletion of a file from the disk
+                filename = parts[1]
+                delete_file(ns, filename)
+
+            elif command == "append":    #This will handle appending data to an already existing file
+                filename = parts[1]
+                filedata = parts[2]
+                append_file(ns, filename, filedata)
+
+            elif command == "quit":      #This will handle disconnections, to stops the loop
+                response = "OK|bye\n"
+                ns.send(response.encode())
+                print("User Program disconnected.")
+                break
+
+            else:                        #This will handle any command which is not recognised
+                response = "ERROR|unknown command\n"
+                ns.send(response.encode())
+
+        except:
+            print("Error receiving message from User Program.")
+            break
+
+except:
+    print("Error accepting connection.")
+
