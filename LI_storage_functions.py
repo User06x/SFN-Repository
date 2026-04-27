af4b770e5edb45fe01e3ab601dd77bae2134c84d
Luka <luka.ivanovski.h63689@mcast.edu.mt>
2026-04-27 07:47:08 +0200

    Update LI_storage_functions.py
    Made a mistake by adding the client code into the functions code, so now i added the actual code for the storage functions which is tweaked to match the User Program's format protocol.
    
    (Its 7 in the morning im half asleep)

--- LI_storage_functions.py
+++ LI_storage_functions.py
@@ -1,61 +1,49 @@
-#This is the my storage client python file for the distributed file system
-#This file will be in charge of accepting connections from the User Program system and handle the file operations.
+#This is my storage functions python file for the distributed file system
+#This module will contain all the file system functions that will be used for the Storage Client file
 
-import socket  #This is used to create the server and handle connections
-from storage_functions import save_file, read_file, delete_file, append_file  #This imports functions from my storage functions module
+import os  #This will be used for the file operations like deleting and renaming 
 
-#This will set up the server socket and will begin to start listening on port number 6001
-ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ss.bind(('127.0.0.1', 6001))
-ss.listen()
+#This saves the received data into a file on the disk, then sends back 'OK' or 'ERROR' depending on the outcome.
+def save_file(conn, filename, filedata):
+    try:
+        f = open(filename, "w")
+        f.write(filedata)
+        f.close()
+        response = "OK|saved\n"
+        conn.send(response.encode())
+    except:
+        response = "ERROR|could not save file\n"
+        conn.send(response.encode())
 
-print("Storage Client is running and waiting for connection...")
+def read_file(conn, filename):
+    #This will read a file from disk and send its content back to the user
+    try:
+        f = open(filename, "r")
+        filedata = f.read()
+        f.close()
+        conn.send(filedata.encode())
+    except:
+        response = "ERROR|could not read file\n"
+        conn.send(response.encode())
 
-try:
-    ns, addr = ss.accept()  #This will wait for User Program to connect to the Storage Client
-    print("User Program connected from", addr)
+def delete_file(conn, filename):
+    #This will delete a file from the disk, and send back 'OK' or 'ERROR' depending on the outcome
+    try:
+        os.remove(filename)
+        response = "OK|deleted\n"
+        conn.send(response.encode())
+    except:
+        response = "ERROR|could not delete file\n"
+        conn.send(response.encode())
 
-    while True:  #This will keep looping and waiting for commands from the User Program module
-        try:
-            message = ns.recv(1024)
-            message = message.decode()
-            message = message.strip()   #This will remove the '\n' at the end of the message
-
-            parts = message.split("|")  #This will split the message by a pipe separator
-            command = parts[0]          #The first part is always the command
-
-            if command == "save":        #This will handle saving a file to the disk
-                filename = parts[1]
-                filedata = parts[2]
-                save_file(ns, filename, filedata)
-
-            elif command == "read":      #This will handle reading a file and sending it back to the user
-                filename = parts[1]
-                read_file(ns, filename)
-
-            elif command == "delete":    #This will handle the deletion of a file from the disk
-                filename = parts[1]
-                delete_file(ns, filename)
-
-            elif command == "append":    #This will handle appending data to an already existing file
-                filename = parts[1]
-                filedata = parts[2]
-                append_file(ns, filename, filedata)
-
-            elif command == "quit":      #This will handle disconnections, to stops the loop
-                response = "OK|bye\n"
-                ns.send(response.encode())
-                print("User Program disconnected.")
-                break
-
-            else:                        #This will handle any command which is not recognised
-                response = "ERROR|unknown command\n"
-                ns.send(response.encode())
-
-        except:
-            print("Error receiving message from User Program.")
-            break
-
-except:
-    print("Error accepting connection.")
-
+def append_file(conn, filename, filedata):
+    #This will append the data to an already existing file, then send back 'OK' or 'ERROR' depending on the outcome
+    try:
+        f = open(filename, "a")
+        f.write(filedata)
+        f.close()
+        response = "OK|appended\n"
+        conn.send(response.encode())
+    except:
+        response = "ERROR|could not append to file\n"
+        conn.send(response.encode())
